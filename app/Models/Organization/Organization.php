2dac67fa2318d277073bc96e7055b767ae2ec2a6
Miller Juma <jumamiller@yahoo.com>
2023-06-29 22:42:29 +0300

    init

---
+++ app/Models/Organization/Organization.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Organization;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+
+class Organization extends BaseTenant implements TenantWithDatabase
+{
+    use HasFactory, HasDatabase, HasDomains;
+
+}
